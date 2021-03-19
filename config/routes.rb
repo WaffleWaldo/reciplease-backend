d651064aa9c4de0063403c47961018741cf13de6
WaffleWaldo <oswaldoortiz0519@gmail.com>
2021-03-19 11:58:59 -0400

    recipe controller is added and show method is up

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # GET /api/v1/recipes/:id
+
+  namespace :api do
+    namespace :v1 do
+      resources :recipes, only: [:show]
+    end
+  end
+
 end
